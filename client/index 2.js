61329928bfa915232d96e9352fd4ff71dae64f9b
yli-yanchen <yli.yanchen@gmail.com>
2023-12-24 00:25:20 -0800

    fix duplicate issue

--- client/index 2.js
+++
@@ -1,9 +0,0 @@
-// The file is to connect the React application to index.html file
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-// import styles from "./stylesheets/styles.css";
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
