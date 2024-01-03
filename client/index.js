34dfabcc9b66d6787725df2c8c90be5b20ff7b97
yli-yanchen <yli.yanchen@gmail.com>
2024-01-03 12:46:50 -0800

    work on some serverside code, but still didn't connected with react

--- client/index.js
+++
@@ -1,8 +0,0 @@
-// The file is to connect the React application to index.html file
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
